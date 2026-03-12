dee533839a8bdb45a5cd6aa50fb39b0443cdec42
NotName465 <not_name00@mail.ru>
2026-03-12 22:11:59 +0300

    Create 12.03.2026 V2 №4.py
    ДЗ по инфе

---
+++ 12.03.2026 V2 №4.py
@@ -0,0 +1,7 @@
+print(f"X   Y   Z   W")
+for x in range(0,2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if(((x and not y) or (x == z) or not w) == 0):
+                    print(f"{x}   {y}   {z}   {w}")
